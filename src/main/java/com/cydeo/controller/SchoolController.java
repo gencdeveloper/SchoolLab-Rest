dd3acb43d0d4b4f56917a5b322aeb54f2942ba12
jamaldemir <jamal@cybertekschool.com>
2023-12-30 11:56:07 -0500

    Parents endpoint is created

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -2,8 +2,10 @@ package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,10 +19,12 @@ public class SchoolController {
 
     private final TeacherService teacherService;
     private final StudentService studentService;
+    private final ParentService parentService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
+        this.parentService = parentService;
     }
 
     @GetMapping("/teachers")
@@ -42,5 +46,27 @@ public class SchoolController {
                 ("students are successfully retrieved."
                 ,studentService.findAll()));
     }
+
+    /*     7 MIN
+           create a parents endpoint where status code is 202
+           additional header has "Parent" , "Returned"
+           and following body structure
+           "parents are successfully retrieved." message
+           code:202
+           success: true
+           and parent data.
+     */
+
+    @GetMapping("/parents")
+    public ResponseEntity<ResponseWrapper> readAllParents(){
+
+        return ResponseEntity
+                            .status(HttpStatus.ACCEPTED)
+                            .header("Parent","Returned")
+                            .body(new ResponseWrapper(true,"parents are successfully retrieved.",
+                                    HttpStatus.ACCEPTED.value(),parentService.findAll()));
+    }
+
+
 
 }
