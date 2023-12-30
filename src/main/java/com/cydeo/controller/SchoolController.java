99d4d820c60af5e9fb6f8ea68befd77e637b5ab1
jamaldemir <jamal@cybertekschool.com>
2023-12-30 12:09:07 -0500

    Address endpoint is created

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,13 +1,16 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.AddressDTO;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.AddressService;
 import com.cydeo.service.ParentService;
 import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,11 +23,13 @@ public class SchoolController {
     private final TeacherService teacherService;
     private final StudentService studentService;
     private final ParentService parentService;
+    private final AddressService addressService;
 
-    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService, ParentService parentService, AddressService addressService) {
         this.teacherService = teacherService;
         this.studentService = studentService;
         this.parentService = parentService;
+        this.addressService = addressService;
     }
 
     @GetMapping("/teachers")
@@ -65,6 +70,24 @@ public class SchoolController {
                             .header("Parent","Returned")
                             .body(new ResponseWrapper(true,"parents are successfully retrieved.",
                                     HttpStatus.ACCEPTED.value(),parentService.findAll()));
+    }
+
+    /*  6MIN
+        create an endpoint for individual address information
+        /address/1 2 3
+        return status code 200
+        "address .. is successfully retrieved" message
+        success true
+        and address information
+     */
+    @GetMapping("/address/{id}")
+    public ResponseEntity<ResponseWrapper> getAddressById(@PathVariable("id") Long id) throws Exception {
+
+        //find the address to return
+        AddressDTO addressDTO = addressService.findById(id);
+
+        return ResponseEntity.ok(new ResponseWrapper("Address "+id+" is successfully retrieved",addressDTO));
+
     }
 
 
