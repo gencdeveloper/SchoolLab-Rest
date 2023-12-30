f7c67b4706e7525d05692bb6625b6a4a57f3f48e
jamaldemir <jamal@cybertekschool.com>
2023-12-30 11:34:53 -0500

    ResponseWrapper and Students endpoint are completed

--- src/main/java/com/cydeo/controller/SchoolController.java
+++ src/main/java/com/cydeo/controller/SchoolController.java
@@ -1,7 +1,10 @@
 package com.cydeo.controller;
 
+import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.TeacherDTO;
+import com.cydeo.service.StudentService;
 import com.cydeo.service.TeacherService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +16,31 @@ public class SchoolController {
     //write a method for teachers and return list of teachers
 
     private final TeacherService teacherService;
+    private final StudentService studentService;
 
-    public SchoolController(TeacherService teacherService) {
+    public SchoolController(TeacherService teacherService, StudentService studentService) {
         this.teacherService = teacherService;
+        this.studentService = studentService;
     }
 
     @GetMapping("/teachers")
     public List<TeacherDTO> allTeachers(){
         return teacherService.findAll();
+    }
+    /*  7MIN
+        create an endpoint for students, where json response includes
+        "students are successfully retrieved." message
+        code:200
+        success:true
+        and student data
+     */
+
+    @GetMapping("/students")
+    public ResponseEntity<ResponseWrapper> readAllStudents(){
+
+        return ResponseEntity.ok(new ResponseWrapper
+                ("students are successfully retrieved."
+                ,studentService.findAll()));
     }
 
 }
